e0cdbad5c0374afb8b07b454c27cf225b86cff6f
Favour <favouronyeke2002@gmail.com>
2023-05-11 15:34:21 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { pageLinks, socialLinks } from "../data";
+
+function Footer() {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} className="footer-link">
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+
+        <li>
+          <a href="#featured" className="footer-link">
+            featured
+          </a>
+        </li>
+      </ul>
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                rel="nonferrer"
+                className="footer-icon"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours owned by Favour
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Tours.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Title from "./Title";
+import { tours } from "../data";
+function Tours() {
+  return (
+    <section className="section" id="tours">
+      <Title title="featured" subtitle="Tours" />
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          const { id, image, date, title, location, period, price } = tour;
+          return (
+            <article className="tour-card" key={id}>
+              <div className="tour-img-container">
+                <img src={image} className="tour-img" alt="" />
+                <p className="tour-date">{date}</p>
+              </div>
+              <div className="tour-info">
+                <h4>{title}</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Cumque vitae tempore voluptatum maxime reprehenderit eum quod
+                  exercitationem fugit, qui corporis.
+                </p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {location}
+                  </p>
+                  <p>{period}</p>
+                  <p>from ${price}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default Tours;
